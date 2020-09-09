612572d3ef309eea6833603624657ad61058bab4
Vladimir <tsvyakh.vova@mail.ru>
2020-09-09 22:51:22 +0700

    Working Zeidel

--- MPIandZEIDEL/Zeidel.cpp
+++ MPIandZEIDEL/Zeidel.cpp
@@ -8,14 +8,14 @@ int Zeidel() {
   const int a = 3;
   const int b = 4;
   double mass[a][b] = {{5, -1, 2, 3}, {-2, -10, 3, -4}, {1, 2, 5, 12}};
-  for (int i = 0; i < a; i++) {   // строка
-    for (int j = 0; j < b; j++) { // столбец
+  for (int i = 0; i < a; i++) {
+    for (int j = 0; j < b; j++) {
       cout << " " << mass[i][j];
     }
     cout << endl;
   }
   double per = 0;
-  for (int i = 0; i < a; i++) { //// получаем единицы на главной диагонали
+  for (int i = 0; i < a; i++) {
     per = mass[i][i];
     for (int j = 0; j < b; j++) {
       mass[i][j] /= per;
@@ -58,7 +58,6 @@ int Zeidel() {
   MaxC[0] = abs(mass[0][1]) + abs(mass[0][2]);
   MaxC[1] = abs(mass[1][0]) + abs(mass[1][2]);
   MaxC[2] = abs(mass[2][0]) + abs(mass[2][1]);
-  cout << MaxC[0] << " " << MaxC[1] << " " << MaxC[2] << endl;
   if ((MaxC[0] >= MaxC[1]) && (MaxC[0] >= MaxC[2])) {
     C = MaxC[0];
   } else if ((MaxC[1] >= MaxC[0]) && (MaxC[1] >= MaxC[2])) {
@@ -66,25 +65,18 @@ int Zeidel() {
   } else if ((MaxC[2] >= MaxC[1]) && (MaxC[2] >= MaxC[0])) {
     C = MaxC[2];
   }
-  cout << C << endl;
-  double massB[a][1];
-  // cout << "Матрица В" << endl;
-  for (int i = 0; i < a; i++) {
-    for (int b = 0; b < 1; b++) {
-      massB[i][0] = mass[i][3];
-      //  cout << massB[i][0];
-    }
-    // cout << endl;
-  }
   double B = 0;
-  if ((massB[0][1] > massB[1][1]) && (massB[0][1] > massB[2][1])) {
-    B = massB[0][1];
-  } else if (massB[1][1] > (massB[0][1] && massB[2][1])) {
-    B = massB[1][1];
-  } else if (massB[2][1] > (massB[0][1] && massB[1][1])) {
-    B = massB[2][1];
+  double MaxB[3];
+  for (int i = 0; i < a; i++) {
+    MaxB[i] = abs(mass[i][3]);
   }
-  cout << B << endl;
+  if ((MaxB[0] >= MaxB[1]) && (MaxB[0] >= MaxB[2])) {
+    B = MaxB[0];
+  } else if ((MaxB[1] >= MaxB[0]) && (MaxB[1] >= MaxB[2])) {
+    B = MaxB[1];
+  } else if ((MaxB[2] >= MaxB[0]) && (MaxB[2] >= MaxB[1])) {
+    B = MaxB[2];
+  }
   double N = (((log(pow(10, -4) * (1 - C) / B))) / (log(C))) + 1;
   cout << "N = " << N << endl;
   return 0;
