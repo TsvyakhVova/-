b497f8a04b3cf804d13eede8e9b4cb69de65131e
Vladimir <tsvyakh.vova@mail.ru>
2020-09-09 23:05:29 +0700

    Add Newton

---
+++ MPDandHordandNewton/Newton.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int Newton() {
+  setlocale(LC_ALL, "rus");
+  int a = 1;
+  int b = 2;
+}
+
+int main() {
+  Newton();
+  return 0;
+}
---
+++ MPDandHordandNewton/Newton.py
@@ -0,0 +1,27 @@
+from sympy import diff, symbols
+
+
+def derivative(pow, per):
+    x = symbols('x')
+    der1 = diff(x**pow-per)
+    return der1
+
+
+def Newton():
+    f = derivative(2, 3)
+    print("Производная x^2-3 = ", f)
+    x = diff(f)
+    X = x
+    print("x0 =", x)
+    for i in range(4):
+        i = (x - ((x**2) - 3) / (X * x))
+        x = i
+        print("%.10f" % (x))
+
+
+def main():
+    Newton()
+
+
+if __name__ == '__main__':
+    main()
